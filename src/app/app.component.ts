4042b0d369c9d1094ef34bc67598f3723b70ad59
Ranjan Panda <jyotiranjan.p14@gmail.com>
2018-03-13 08:50:07 +0530

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:"ap-app",
+  // template:"<h2>Welcome to Angular Class</h2>"
+  // template:`
+  // <h1>Welcome to Angular Class</h1>
+  // <h2>We are tagetting to learn Angular 2</h2>
+  // <p>Hey {{title}}, how are you doing today</p>
+  // `
+  templateUrl:"app.component.html",
+  styleUrls:[
+    "app.component.css"
+  ]
+})
+export class AppComponent {
+  title:string="Kailasji, GautamJi";
+  Names=[
+    "Ram", "Manoj","Amol","Kailas","Gautam"
+  ];
+}
